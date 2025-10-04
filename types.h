c82c29fdf526557cfff6e4b9c75438e68dedc8d1
Aman <aman.swar001@gmail.com>
2025-10-04 12:14:14 +0530

    feat : add typedef

---
+++ types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cuda_fp16.h>
+#include <cuda_bf16.hpp>
+#include <cuda_fp8.h>
+
+#define FP32 float
+#define FP16 half
+#define BF16 __nv_bfloat16
+#define FP8 __nv_fp8_e4m3
+
+#define INT32 int
+#define INT16 std::int16_t
+#define INT8 std::int8_t
+
